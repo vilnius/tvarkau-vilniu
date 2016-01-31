a2af187ef8ab16c92f6fc813035f711dc85986bf
Karolis Vycius <karolis.vycius@adform.com>
2016-01-31 10:53:11 +0200

    Map info window views

--- app/src/main/java/lt/vilnius/tvarkau/fragments/ProblemsMapFragment.java
+++ app/src/main/java/lt/vilnius/tvarkau/fragments/ProblemsMapFragment.java
@@ -1,6 +1,7 @@
 package lt.vilnius.tvarkau.fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -12,8 +13,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.HashMap;
+
+import lt.vilnius.tvarkau.ProblemDetailActivity;
 import lt.vilnius.tvarkau.R;
 import lt.vilnius.tvarkau.entity.Problem;
 import lt.vilnius.tvarkau.factory.DummyProblems;
@@ -23,7 +28,7 @@ import lt.vilnius.tvarkau.views.adapters.MapsInfoWindowAdapter;
 /**
  * Created by Karolis Vycius on 2016-01-13.
  */
-public class ProblemsMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+public class ProblemsMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     protected static final LatLng VILNIUS_LAT_LNG = new LatLng(54.687157, 25.279652);
     protected static final int GPS_PERMISSION_REQUEST_CODE = 11;
@@ -32,6 +37,8 @@ public class ProblemsMapFragment extends SupportMapFragment implements OnMapRead
 
     protected static BitmapDescriptor inProgressMarker;
     protected static BitmapDescriptor doneMarker;
+
+    protected HashMap<String, Problem> problemHashMap = new HashMap<>();
 
     public static ProblemsMapFragment getInstance() {
         return new ProblemsMapFragment();
@@ -65,16 +72,24 @@ public class ProblemsMapFragment extends SupportMapFragment implements OnMapRead
     @Override
     public void onMapReady(GoogleMap map) {
         googleMap = map;
+        map.setOnInfoWindowClickListener(this);
 
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(VILNIUS_LAT_LNG, 10f));
 
         requestGPSPermission();
 
-        map.setInfoWindowAdapter(new MapsInfoWindowAdapter(getActivity().getLayoutInflater()));
 
         for (Problem problem : DummyProblems.getProblems()) {
+            String problemStringId = String.valueOf(problem.getId());
+
+            // Hack: Google Map don't have setData method.
+            // There is no easy way to get problem from marker.
+            // Set problem id as marker title and keep hashmap of problems with ids
+            problemHashMap.put(problemStringId, problem);
+
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(problem.getLatLng());
+            markerOptions.title(problemStringId);
 
             switch (problem.getStatusCode()) {
                 case Problem.STATUS_IN_PROGRESS:
@@ -87,12 +102,15 @@ public class ProblemsMapFragment extends SupportMapFragment implements OnMapRead
 
             map.addMarker(markerOptions);
         }
+
+        map.setInfoWindowAdapter(new MapsInfoWindowAdapter(getActivity(), problemHashMap));
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == GPS_PERMISSION_REQUEST_CODE) {
-            if (permissions.length == 1 &&
+            if (permissions.length >= 1 &&
                     permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Try/catch is used only to suppress permission grant error. It's impossible scenario.
@@ -104,5 +122,14 @@ public class ProblemsMapFragment extends SupportMapFragment implements OnMapRead
                 }
             }
         }
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        int problemId = problemHashMap.get(marker.getTitle()).getId();
+
+        Intent intent = ProblemDetailActivity.getStartActivityIntent(getActivity(), problemId);
+
+        startActivity(intent);
     }
 }
