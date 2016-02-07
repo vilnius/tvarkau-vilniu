17763916c3757662696afe1e5f28653637698473
Karolis Vycius <karolis.vycius@adform.com>
2016-02-07 11:53:13 +0200

    Add onClickListeners for all home screen buttons

--- app/src/main/java/lt/vilnius/tvarkau/MainActivity.java
+++ app/src/main/java/lt/vilnius/tvarkau/MainActivity.java
@@ -2,34 +2,16 @@ package lt.vilnius.tvarkau;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-import butterknife.Bind;
-import javax.inject.Inject;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import lt.vilnius.tvarkau.utils.SharedPrefsManager;
-import lt.vilnius.tvarkau.network.service.IssueService;
 
 
 /**
- * An activity representing a list of Problems. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link ProblemDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
+ * An activity representing a home screen
  */
 public class MainActivity extends BaseActivity {
-    @Bind(R.id.home_btn_sign_in)
-    TextView mLoginButton;
-
-    SharedPrefsManager prefsManager;
-    @Inject
-    IssueService issueService;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +19,12 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.home);
 
         ButterKnife.bind(this);
-
-        prefsManager = SharedPrefsManager.getInstance(this);
-        handleSignInButton();
     }
-
-    private void handleSignInButton() {
-        if (!prefsManager.getIsUserAnonymous()) {
-            mLoginButton.setVisibility(View.GONE);
-        }
-    }
-
 
     protected void startNewActivity(Class<?> cls) {
         Intent intent = new Intent(this, cls);
 
         startActivity(intent);
-    }
-
-    @OnClick(R.id.home_btn_sign_in)
-    protected void onSignInButtonClicked() {
-        startNewActivity(SignInActivity.class);
     }
 
     @OnClick(R.id.home_report_problem)
@@ -70,6 +37,11 @@ public class MainActivity extends BaseActivity {
         startNewActivity(ProblemsListActivity.class);
     }
 
+    @OnClick(R.id.home_my_problems)
+    protected void onMyProblemsClicked() {
+        Toast.makeText(this, "This should be properly implemented", Toast.LENGTH_SHORT).show();
+    }
+
     @OnClick(R.id.home_map_of_problems)
     protected void onProblemsMapClicked() {
         startNewActivity(ProblemsMapActivity.class);
@@ -79,4 +51,11 @@ public class MainActivity extends BaseActivity {
     protected void onMyProfileClicked() {
         startNewActivity(MyProfileActivity.class);
     }
+
+    @OnClick(R.id.home_about)
+    protected void onAboutClicked() {
+        Toast.makeText(this, "This should be implemented", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
