6cce72955fce461dffd37cdca39dbbd43ad512bc
Karolis Vycius <k.vycius@gmail.com>
2016-05-12 19:47:45 +0300

    Same animation for opening report details

--- app/src/main/java/lt/vilnius/tvarkau/views/adapters/ProblemsListAdapter.java
+++ app/src/main/java/lt/vilnius/tvarkau/views/adapters/ProblemsListAdapter.java
@@ -2,10 +2,6 @@ package lt.vilnius.tvarkau.views.adapters;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -16,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 
 import com.bumptech.glide.Glide;
 
@@ -72,26 +67,9 @@ public class ProblemsListAdapter
             @Override
             public void onClick(View view) {
                 Intent intent = ProblemDetailActivity.getStartActivityIntent(activity, holder.item.id);
-                Drawable drawable = holder.thumbView.getDrawable();
 
-                Bundle bundle;
-                if(holder.thumbView.getVisibility() == View.VISIBLE) {
-                    Bitmap bitmap;
-
-                    if (drawable instanceof BitmapDrawable) {
-                        bitmap = ((BitmapDrawable) drawable).getBitmap();
-                    } else {
-                        bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-                        Canvas canvas = new Canvas(bitmap);
-                        drawable.draw(canvas);
-                    }
-
-                    bundle = ActivityOptionsCompat.makeThumbnailScaleUpAnimation(holder.thumbView, bitmap, 0, 0).toBundle();
-                } else {
-                    bundle = ActivityOptionsCompat.makeScaleUpAnimation(view, 0, 0,
+                Bundle bundle = ActivityOptionsCompat.makeScaleUpAnimation(view, 0, 0,
                             view.getWidth(), view.getHeight()).toBundle();
-                }
-
 
                 ActivityCompat.startActivity(activity, intent, bundle);
             }
