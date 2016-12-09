912ae039bd4658e653834eca2c0998157c39cb97
Karolis Vycius <k.vycius@gmail.com>
2016-12-09 23:49:56 +0200

    Migrate About screen to Kotlin and show only version name

---
+++ app/src/main/java/lt/vilnius/tvarkau/AboutActivity.kt
@@ -0,0 +1,110 @@
+package lt.vilnius.tvarkau
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
+import butterknife.ButterKnife
+import butterknife.OnClick
+import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.app_bar.*
+import lt.vilnius.tvarkau.utils.DeviceUtils
+import lt.vilnius.tvarkau.utils.FormatUtils
+import lt.vilnius.tvarkau.utils.GlobalConsts
+
+class AboutActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_about)
+        ButterKnife.bind(this)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        version_code.text = DeviceUtils.appVersion
+
+        Linkify.addLinks(thanks_to_contributors, Linkify.WEB_URLS)
+        FormatUtils.removeUnderlines(thanks_to_contributors)
+        thanks_to_contributors.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    @OnClick(R.id.rate_app)
+    fun onRateAppClick() {
+        val uri = Uri.parse("market://details?id=$packageName")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=$packageName")))
+        }
+
+    }
+
+    @OnClick(R.id.contribute_with_code)
+    fun onClickContributeWithCode() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(GlobalConsts.CODE_FOR_VILNIUS_TRELLO))
+        startActivity(intent)
+    }
+
+    @OnClick(R.id.report_bug)
+    fun onReportBugClick() {
+        openEmail(GlobalConsts.CODE_FOR_VILNIUS_EMAIL)
+    }
+
+    @OnClick(R.id.visit_our_website)
+    fun onVisitOurWebsiteClick() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(GlobalConsts.CODE_FOR_VILNIUS_WEBSITE))
+        startActivity(intent)
+    }
+
+    @OnClick(R.id.facebook_page)
+    fun onFacebookPageClick() {
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/" + GlobalConsts.CODE_FOR_VILNIUS_FACEBOOK_ID)))
+        } catch (e: Exception) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(GlobalConsts.CODE_FOR_VILNIUS_FACEBOOK)))
+        }
+    }
+
+    @OnClick(R.id.meetup_page)
+    fun onMeetupPageClick() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(GlobalConsts.CODE_FOR_VILNIUS_MEETUP))
+        startActivity(intent)
+    }
+
+    @OnClick(R.id.phone_select_view)
+    fun onPhoneViewClick() {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: ${GlobalConsts.VILNIUS_MUNICIPALITY_PHONE}"))
+        startActivity(intent)
+    }
+
+    @OnClick(R.id.municipality_email)
+    fun onMunicipalityEmailClick() {
+        openEmail(GlobalConsts.VILNIUS_MUNICIPALITY_EMAIL)
+    }
+
+    private fun openEmail(emailAddress: String) {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:" + emailAddress)
+        intent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.contact_email_subject) + " (" +
+                DeviceUtils.deviceInfo + ", " + resources.getString(R.string.app_version) + " " +
+                DeviceUtils.appVersion + ")")
+        if (intent.resolveActivity(this.packageManager) != null) {
+            startActivity(intent)
+        } else {
+            AlertDialog.Builder(this, R.style.MyDialogTheme)
+                    .setMessage(resources.getString(R.string.send_email_to) + " " + emailAddress)
+                    .setPositiveButton(R.string.ok) { dialog, whichButton -> super@AboutActivity.onBackPressed() }
+                    .create()
+                    .show()
+        }
+    }
+}
