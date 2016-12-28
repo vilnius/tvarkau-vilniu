9772bc5e18895cf89c00625a1160de50489c51f8
Martynas Jurkus <martynas@vinted.com>
2016-12-28 13:36:00 +0200

    Update regexp to avoid using redundant escape symbols

--- app/src/main/java/lt/vilnius/tvarkau/utils/TextUtils.kt
+++ app/src/main/java/lt/vilnius/tvarkau/utils/TextUtils.kt
@@ -8,7 +8,7 @@ import kotlin.text.RegexOption.IGNORE_CASE
 
 object TextUtils {
 
-    private val PROBLEM_ID_REGEXP = "[A-Z]\\d{2}-\\d+\\/\\d+\\(\\S+?\\)".toRegex(IGNORE_CASE)
+    private val PROBLEM_ID_REGEXP = """[A-Z]\d{2}-\d+/\d+\(\S+?\)""".toRegex(IGNORE_CASE)
 
     fun findReportIdOccurrences(source: String): List<String> {
         return PROBLEM_ID_REGEXP.findAll(source).map { it.value }.toList()
