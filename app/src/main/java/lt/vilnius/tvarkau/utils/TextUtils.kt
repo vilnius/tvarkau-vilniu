0f1b381bd64d03159ba74a9ac88eb86d86254885
Martynas Jurkus <martynas.jurkus@gmail.com>
2016-12-28 13:55:46 +0200

    Improve report id lookup (#144)

---
+++ app/src/main/java/lt/vilnius/tvarkau/utils/TextUtils.kt
@@ -0,0 +1,16 @@
+package lt.vilnius.tvarkau.utils
+
+import kotlin.text.RegexOption.IGNORE_CASE
+
+/**
+ * @author Martynas Jurkus
+ */
+
+object TextUtils {
+
+    private val PROBLEM_ID_REGEXP = """[A-Z]\d{2}-\d+/\d+\(\S+?\)""".toRegex(IGNORE_CASE)
+
+    fun findReportIdOccurrences(source: String): List<String> {
+        return PROBLEM_ID_REGEXP.findAll(source).map { it.value }.toList()
+    }
+}
---
+++ app/src/test/java/lt/vilnius/tvarkau/utils/TextUtilTest.kt
@@ -0,0 +1,60 @@
+package lt.vilnius.tvarkau.utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * @author Martynas Jurkus
+ */
+@RunWith(RobolectricTestRunner::class)
+class TextUtilTest {
+
+    @Test
+    fun findSingleProblemId() {
+        val single = "We have a huge problem here E50-7476/16(3.2.47E-SM4)."
+        val problemIdOccurrences = TextUtils.findReportIdOccurrences(single)
+
+        assertEquals(1, problemIdOccurrences.size.toLong())
+        assertEquals("E50-7476/16(3.2.47E-SM4)", problemIdOccurrences[0])
+    }
+
+    @Test
+    fun findMultipleProblemIds() {
+        val multiple = "We have tow huge problems this one E50-7476/16(3.2.47E-SM4) and " +
+                "this one A51-111/16(3.2.47E-SM4)."
+        val problemIdOccurrences = TextUtils.findReportIdOccurrences(multiple)
+
+        assertEquals(2, problemIdOccurrences.size.toLong())
+        assertEquals("E50-7476/16(3.2.47E-SM4)", problemIdOccurrences[0])
+        assertEquals("A51-111/16(3.2.47E-SM4)", problemIdOccurrences[1])
+    }
+
+    @Test
+    fun skipMalformedProblemIds() {
+        val single = "Malformed E507476/16(3.2.47E-SM4) and  E50-7476/16(3.2.47E-SM4."
+        val problemIdOccurrences = TextUtils.findReportIdOccurrences(single)
+
+        assertTrue(problemIdOccurrences.isEmpty())
+    }
+
+    @Test
+    fun includeLowerCaseProblemIds() {
+        val single = "We have a huge problem here e50-7476/16(3.2.47E-SM4)."
+        val problemIdOccurrences = TextUtils.findReportIdOccurrences(single)
+
+        assertEquals(1, problemIdOccurrences.size)
+    }
+
+    @Test
+    fun checkNonGreedy() {
+        val text = "Two sameE50-7476/16(3.2.47E-SM4) ids zzzE50-7476/16(3.2.47E-SM4))))"
+        val occurrences = TextUtils.findReportIdOccurrences(text)
+
+        assertEquals(2, occurrences.size)
+        assertEquals("E50-7476/16(3.2.47E-SM4)", occurrences[0])
+        assertEquals("E50-7476/16(3.2.47E-SM4)", occurrences[1])
+    }
+}
