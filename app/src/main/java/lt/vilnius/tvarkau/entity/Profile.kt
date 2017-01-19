ff2fd5a223c53e84b063d83e3e77ed3d748f68ec
Martynas Jurkus <martynas@vinted.com>
2017-01-19 23:13:06 +0200

    Drop birthday field in favour of personal code

--- app/src/main/java/lt/vilnius/tvarkau/entity/Profile.kt
+++ app/src/main/java/lt/vilnius/tvarkau/entity/Profile.kt
@@ -3,14 +3,10 @@ package lt.vilnius.tvarkau.entity
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
-import org.threeten.bp.LocalDate
-
 
 data class Profile(
         @SerializedName("Profile_Name")
         var name: String? = null,
-        @SerializedName("Profile_Birthday")
-        var birthday: LocalDate? = null,
         @SerializedName("Profile_Email")
         var email: String? = null,
         @SerializedName("Profile_Mobile_Phone")
