b062573803e453cd4b920f31d0c9f320b265bfd2
Martynas Jurkus <martynas.jurkus@gmail.com>
2017-01-22 20:32:25 +0200

    Do not update report date time unless it parking violation (#157)
    Fixes #156; Do not update report date time unless it parking violation

--- app/src/main/java/lt/vilnius/tvarkau/mvp/views/NewReportView.kt
+++ app/src/main/java/lt/vilnius/tvarkau/mvp/views/NewReportView.kt
@@ -2,6 +2,7 @@ package lt.vilnius.tvarkau.mvp.views
 
 import lt.vilnius.tvarkau.entity.Profile
 import lt.vilnius.tvarkau.utils.FieldAwareValidator
+import java.io.File
 
 /**
  * @author Martynas Jurkus
@@ -19,4 +20,8 @@ interface NewReportView {
     fun showError(error: Throwable)
 
     fun showSuccess()
+
+    fun fillReportDateTime(dateTime: String)
+
+    fun displayImages(imageFiles: List<File>)
 }
