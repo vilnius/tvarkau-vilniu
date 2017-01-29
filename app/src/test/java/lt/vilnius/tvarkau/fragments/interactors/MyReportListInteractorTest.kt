81fd9834cb36f740e4f66cfc35b1634d7ecaa293
Martynas Jurkus <martynas@vinted.com>
2017-01-29 14:44:04 +0200

    Restore my reports sorting

--- app/src/test/java/lt/vilnius/tvarkau/fragments/interactors/MyReportListInteractorTest.kt
+++ app/src/test/java/lt/vilnius/tvarkau/fragments/interactors/MyReportListInteractorTest.kt
@@ -6,9 +6,11 @@ import lt.vilnius.tvarkau.backend.LegacyApiService
 import lt.vilnius.tvarkau.entity.Problem
 import lt.vilnius.tvarkau.wrapInResponse
 import org.junit.Test
+import org.threeten.bp.LocalDateTime
 import rx.Observable
 import rx.Single.just
 import rx.schedulers.Schedulers
+import kotlin.test.assertEquals
 
 /**
  * @author Martynas Jurkus
@@ -89,5 +91,36 @@ class MyReportListInteractorTest {
 
         verify(api, times(3)).getProblem(any())
         verify(idProvider).removeReportId("2")
+    }
+
+    @Test
+    fun getReports_returnsSortedByEntryDate() {
+        val problemsIds = listOf("1", "2", "3")
+        val problems = listOf(
+                Problem(
+                        id = "any",
+                        entryDate = LocalDateTime.of(2017, 1, 1, 12, 3)
+                ),
+                Problem(
+                        id = "any_1",
+                        entryDate = LocalDateTime.of(2017, 1, 1, 12, 2)
+                ),
+                Problem(
+                        id = "any_2",
+                        entryDate = LocalDateTime.of(2017, 1, 1, 12, 1)
+                )
+        )
+
+        whenever(idProvider.getReportIds()).thenReturn(just(problemsIds))
+        whenever(api.getProblem(any()))
+                .thenReturn(problems[0].wrapInResponse)
+                .thenReturn(problems[1].wrapInResponse)
+                .thenReturn(problems[2].wrapInResponse)
+
+        val sorted = fixture.getProblems(1)
+                .test()
+                .onNextEvents.first()
+
+        assertEquals(sorted[0], problems[2])
     }
 }
