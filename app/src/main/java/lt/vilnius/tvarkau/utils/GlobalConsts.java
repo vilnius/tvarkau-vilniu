a1dbac0de350579609b9b0f737ed8a15743ccbde
Martynas Jurkus <martynas.jurkus@gmail.com>
2017-02-27 09:29:44 +0200

    Add privacy policy link (#177)

--- app/src/main/java/lt/vilnius/tvarkau/utils/GlobalConsts.java
+++ app/src/main/java/lt/vilnius/tvarkau/utils/GlobalConsts.java
@@ -4,7 +4,7 @@ public class GlobalConsts {
     public final static String TAG_SINGLE_PROBLEM_MAP_FRAGMENT = "singleProblemMapFragment";
     public final static String TAG_MULTIPLE_PROBLEMS_MAP_FRAGMENT = "multipleProblemsMapFragment";
 
-    public final static String KEY_MAP_FRAGMENT= "keyMapFragment";
+    public final static String KEY_MAP_FRAGMENT = "keyMapFragment";
 
     public final static String CITY_VILNIUS = "Vilnius";
 
@@ -16,6 +16,8 @@ public class GlobalConsts {
     public final static String CODE_FOR_VILNIUS_EMAIL = "tvarkau.vilniu@codeforvilnius.lt";
     public final static String VILNIUS_MUNICIPALITY_EMAIL = "e.vicemeras@vilnius.lt";
     public final static String VILNIUS_MUNICIPALITY_PHONE = "1664";
+    public final static String PRIVACY_POLICY_PAGE = "http://www.vilnius.lt/index.php?303816849";
 
-    private GlobalConsts() {}
+    private GlobalConsts() {
+    }
 }
