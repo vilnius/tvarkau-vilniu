fae302409175030408a00b3f7d6d64f7c2b494d3
Karolis Vycius <k.vycius@gmail.com>
2017-03-04 10:24:11 +0200

    MixPanel implementation

--- app/src/main/java/lt/vilnius/tvarkau/dagger/module/AnalyticsModule.java
+++ app/src/main/java/lt/vilnius/tvarkau/dagger/module/AnalyticsModule.java
@@ -2,6 +2,8 @@ package lt.vilnius.tvarkau.dagger.module;
 
 import android.app.Application;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -12,9 +14,17 @@ import lt.vilnius.tvarkau.analytics.RemoteAnalytics;
 @Module
 public class AnalyticsModule {
 
+    private final String projectToken = "8f77b0f6ecba7677aec235170e8eee4f";
+
     @Provides
     @Singleton
-    Analytics providesAnalytics(Application application) {
-        return new RemoteAnalytics(application.getApplicationContext());
+    MixpanelAPI providesMixPanel(Application application) {
+        return MixpanelAPI.getInstance(application, projectToken);
+    }
+
+    @Provides
+    @Singleton
+    Analytics providesAnalytics(Application application, MixpanelAPI mixpanelAPI) {
+        return new RemoteAnalytics(application.getApplicationContext(), mixpanelAPI);
     }
 }
