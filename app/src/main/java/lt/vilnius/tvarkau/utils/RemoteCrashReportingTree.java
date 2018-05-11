fe574505fc7b4da1c0177bffab64816f77bed25f
Karolis Vycius <k.vycius@gmail.com>
2018-05-11 20:24:07 +0300

    Crashlytics instead of deprecated Firebase crash

--- app/src/main/java/lt/vilnius/tvarkau/utils/RemoteCrashReportingTree.java
+++ app/src/main/java/lt/vilnius/tvarkau/utils/RemoteCrashReportingTree.java
@@ -1,30 +1,31 @@
 package lt.vilnius.tvarkau.utils;
 
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.google.firebase.crash.FirebaseCrash;
+import com.crashlytics.android.Crashlytics;
 
 import timber.log.Timber;
 
 public class RemoteCrashReportingTree extends Timber.Tree {
 
     @Override
-    protected void log(int priority, String tag, String message, Throwable t) {
+    protected void log(int priority, String tag, @NonNull String message, Throwable t) {
         if (priority == Log.VERBOSE || priority == Log.DEBUG) {
             return;
         }
 
         String messageWithTag = tag + ": " + message;
 
-        FirebaseCrash.log(messageWithTag);
+        Crashlytics.log(messageWithTag);
 
         if (t != null) {
-            FirebaseCrash.report(t);
+            Crashlytics.logException(t);
         } else if (priority > Log.WARN) {
             Throwable throwable = new Throwable(messageWithTag);
 
-            FirebaseCrash.report(throwable);
+            Crashlytics.logException(throwable);
         }
     }
 }
