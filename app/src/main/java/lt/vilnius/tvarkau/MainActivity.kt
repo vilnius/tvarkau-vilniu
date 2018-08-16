f171b62cc6db70da068851560ed6076bff1452b2
Martynas Jurkus <martynas@vinted.com>
2018-08-16 15:08:49 +0300

    Remove guest token fetching from legacy app

--- app/src/main/java/lt/vilnius/tvarkau/MainActivity.kt
+++ app/src/main/java/lt/vilnius/tvarkau/MainActivity.kt
@@ -7,7 +7,6 @@ import lt.vilnius.tvarkau.activity.available
 import lt.vilnius.tvarkau.activity.googlePlayServicesAvailability
 import lt.vilnius.tvarkau.activity.resolutionDialog
 import lt.vilnius.tvarkau.activity.resultCode
-import lt.vilnius.tvarkau.auth.SessionToken
 import lt.vilnius.tvarkau.dagger.component.ActivityComponent
 import lt.vilnius.tvarkau.dagger.component.MainActivityComponent
 import lt.vilnius.tvarkau.navigation.BottomNavigationController
@@ -17,8 +16,6 @@ class MainActivity : BaseActivity() {
 
     @Inject
     lateinit var bottomNavigationController: BottomNavigationController
-    @Inject
-    lateinit var sessionToken: SessionToken
 
     private var googlePlayServicesResolutionDialog: Dialog? = null
 
@@ -34,8 +31,6 @@ class MainActivity : BaseActivity() {
         setSupportActionBar(toolbar)
 
         bottomNavigationController.onCreate(savedInstanceState == null)
-
-        sessionToken.refreshGuestToken().subscribe()
     }
 
     override fun onStart() {
